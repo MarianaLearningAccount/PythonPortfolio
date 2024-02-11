8e3ffddf1933dee099a0f7092d14c1a675cf6b35
MarianaUserGit <maroleinic@gmail.com>
2024-02-11 16:01:20 +0200

    add check and reset buttons, add page navigation

--- app.py
+++ app.py
@@ -1,31 +1,44 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 
 import random
 
 app = Flask(__name__)
 
+@app.route('/', methods = ['GET', 'POST'])
+def home():
+  return render_template('index.html')
 
-global comp_num, number_of_guesses
-comp_num = random.randint(1, 100)
-number_of_guesses = 0 
+
+random_num = None
+attempts = 0
+
+def reset_game():
+    global random_num, attempts
+    random_num = random.randint(1, 100)
+    attempts = 0
 
 @app.route('/program1', methods = ['GET', 'POST'])
 def program1():
-    global comp_num, number_of_guesses
-    message = ''
-    if request.method == 'POST':
-        form = request.form
-        user_guess = int(form['guess'])
-        if user_guess != comp_num:
-          number_of_guesses += 1  
-        if comp_num == user_guess:
-           message = 'Bingo! You guessed the number! Random number:' 
-           return render_template('program1.html', message = message, random = comp_num, number = number_of_guesses)
-        elif comp_num > user_guess:
-            message = "Ups! Try again and choose a higher number!"
-        else:
-           message = "Ups! Try again and choose a lower number!"   
-    return render_template('program1.html', message = message, number = number_of_guesses)
+    global random_num, attempts
+    if request.method == 'GET':
+       reset_game()
+       return render_template('program1.html')
+    elif request.method == 'POST':
+       user_guess = int(request.form['guess']) 
+       attempts += 1
+       if user_guess == random_num:
+          message = f'Bingo! You guessed the number in {attempts} attempts! Random number: {random_num}' 
+          reset_game()
+       elif user_guess < random_num:
+          message = "Ups! Try again and choose a higher number!"
+       else:
+          message = "Ups! Try again and choose a lower number!"   
+    return render_template('program1.html', message = message, number = attempts)
+    
+@app.route('/program1/reset', methods=['POST'])   
+def reset():
+   reset_game()
+   return redirect('/program1') 
 
 @app.route('/program2', methods = ['GET', 'POST'])
 def program2():
@@ -35,6 +48,7 @@ def program2():
         form = request.form
         user_time = form['time']
         time_list = user_time.split(" ")
+
         if time_list[1].lower() == "pm":
          digit_list = time_list[0].split(":")
          h = int(digit_list[0])
@@ -43,6 +57,7 @@ def program2():
          else:
           converted_time = h + 12
          result = str(converted_time) + ":" + str(digit_list[1])
+
         if time_list[1].lower() == "am":
          digit_list = time_list[0].split(":")
          h = int(digit_list[0])
@@ -51,6 +66,7 @@ def program2():
          else:
           converted_time = h
          result = str(converted_time) + ":" + str(digit_list[1])
+
     return render_template('program2.html', result = result)
 
 if __name__ == '__main__':
